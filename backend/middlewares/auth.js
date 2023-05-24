5fb7fcc1ea9804916b900a7299ca080649039403
Sasha Bolgova <aleks.kostyleva@yandex.ru>
2023-05-24 11:22:45 +0300

    auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,13 +3,12 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!token) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
 
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
